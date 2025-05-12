5b65d34b03fe09abd9fd1642a4235707c9d770ee
saleem0310 <saleemmohammad0310@gmail.com>
2025-05-12 06:44:52 +0530

    my first commit

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This app is all about ai website to generate ai pictures
+          
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hi this is saleem creating Ai image generator App
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
